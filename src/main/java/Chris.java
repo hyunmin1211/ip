f1b35bdc54fc265e823328b1874d5c99df3f6299
hyunmin <hyunmin1211@gmail.com>
2026-08-18 16:06:10 +0800

    Complete Level 0 rename, greeting, and exit

---
+++ src/main/java/Chris.java
@@ -0,0 +1,18 @@
+public class Chris {
+    public static void main(String[] args) {
+        String banner = "  ____ _          _     \n"
+                + " / ___| |__  _ __(_)___ \n"
+                + "| |   | '_ \\| '__| / __|\n"
+                + "| |___| | | | |  | \\__ \\\n"
+                + " \\____|_| |_|_|  |_|___/";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Chris.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
