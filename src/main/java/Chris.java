649e6f106812d4fce928427b28e4bda59403baba
hyunmin <hyunmin1211@gmail.com>
2026-08-20 14:28:58 +0800

    Level 1 changes - echo input

--- src/main/java/Chris.java
+++ src/main/java/Chris.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Chris {
     public static void main(String[] args) {
         String banner = "  ____ _          _     \n"
@@ -12,7 +14,21 @@ public class Chris {
         System.out.println("Hello! I'm Chris.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(input);
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
